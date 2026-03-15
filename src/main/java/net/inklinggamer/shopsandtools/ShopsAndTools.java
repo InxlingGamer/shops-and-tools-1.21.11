bbf4b2573f8580071abf67b8a0f93b9fae784aba
InxlingGamer <mlgsnipingking@gmail.com>
2026-03-14 22:02:19 -0400

    Initial commit

---
+++ src/main/java/net/inklinggamer/shopsandtools/ShopsAndTools.java
@@ -0,0 +1,17 @@
+package net.inklinggamer.shopsandtools;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class ShopsAndTools implements ModInitializer {
+	public static final String MOD_ID = "shopsandtools";
+	
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
---
+++ src/main/java/net/inklinggamer/shopsandtools/ShopsAndToolsClient.java
@@ -0,0 +1,10 @@
+package net.inklinggamer.shopsandtools;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ShopsAndToolsClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
